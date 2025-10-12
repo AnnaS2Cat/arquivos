f0dbf99e8d041a81c3c29fa1a57935f995a116d4
AnnaS2Cat <annagraduacaouerj@gmail.com>
2025-10-12 08:28:46 -0300

    Criar questão2.c

---
+++ questão2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char string[32];
+    FILE* file = fopen("ex1.txt", "r");
+
+    int read, i = 0;
+    while((read = fgetc(file)) != EOF){
+        string[i++] = read;
+    }
+
+    string[i] = '\0';
+    printf("%s", string);
+    fclose(file);
+}
