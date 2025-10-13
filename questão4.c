6be4173da0b27f925fbe601c9b3ce9b9f31857aa
AnnaS2Cat <annagraduacaouerj@gmail.com>
2025-10-12 21:42:49 -0300

    Update questão4.c

--- questão4.c
+++ questão4.c
@@ -3,24 +3,23 @@
 
 int main(void)
 {   
-    FILE* file = fopen("ex4.txt", "w");
-    if (file == NULL){
-        printf("não conseguiu criar o arquivo\n");
+    FILE* file = fopen("q4.txt", "w"); //tenta abrir ou criar um arquivo chamado q4.txt no modo "w" = write mode, cria um novo arquivo ou sobrescreve o existente (apaga o conteúdo anterior)
+    if (file == NULL){  // se fopen() falhar, por exemplo, falta de permissão, diretório inexistente, etc, ela retorna NULL
+        printf("arquivo não criado \n");
         return 1;
     }
-
-    for(int i = 0; i<10; i++){
-        char string[32];
+    for(int i = 0; i<10; i++){ 
+        char string[32]; //guarda uma palavra digitada pelo usuário até 31 caracteres + \0 final
         printf("Diga uma palavra: ");
         scanf("%25s", string);
 
         int size = strlen(string);
 
         for (int i = 0; i < size; i++){
-            fputc(string[i], file);
+            fputc(string[i], file); //grava um caractere por vez no arquivo
         }
-        fprintf(file, "\n");
+        fprintf(file, "\n"); //cada palavra fica separada por linha no arquivo
     }
 
-    fclose(file);
+    fclose(file); //fecha o arquivo e garante que tudo que foi escrito em memória seja realmente salvo em disco
 }
