9faa8c92afb521b74368b3a502ac003ce0eebc12
AnnaS2Cat <annagraduacaouerj@gmail.com>
2025-10-28 09:54:02 -0300

    Update questão2.c

--- questão2.c
+++ questão2.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(void)
-{
-    char string[32];
-    FILE* file = fopen("ex1.txt", "r");
+int main(void) {
+    FILE *arquivo;            //ponteiro para o arquivo
+    char texto[26];           
+    int i = 0;                
+    char c;                   //variável que armazena cada caractere lido
 
-    int read, i = 0;
-    while((read = fgetc(file)) != EOF){
-        string[i++] = read;
+    arquivo = fopen("texto.txt", "r"); //abre o arquivo para leitura
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo!\n");
+        return 1;
     }
 
-    string[i] = '\0';
-    printf("%s", string);
-    fclose(file);
+    // Lê o arquivo caractere por caractere até o final (EOF)
+    while ((c = fgetc(arquivo)) != EOF && i < 25) {           //fgetc(arquivo) lê um caractere do arquivo e armazena em c caso o arquivo não chegue ao final e o i<25
+        texto[i] = c;   //armazena o caractere na string
+        i++;           
+    }
+
+    texto[i] = '\0'; // Finaliza a string com o caractere nulo
+    fclose(arquivo);
+    printf("String lida do arquivo: %s\n", texto);
+    return 0;
 }
