fe53a73902f7f0262160caad99117ec0e2c13345
AnnaS2Cat <annagraduacaouerj@gmail.com>
2025-10-28 11:21:18 -0300

    Refactor string writing and file handling in questao5.c

--- questao5.c
+++ questao5.c
@@ -1,26 +1,64 @@
+//para a questao 1
 #include <stdio.h>
 #include <string.h>
 
-void escreva_string (FILE* f, char* str);
+void escreva_string(FILE* f, char* str) {    //recebe o arquivo f e uma string
+    for (int i = 0; i < strlen(str); i++) {
+        fputc(str[i], f); //bota os elementos da string no arquivo um a um
+    }
+}
 
-int main()
-{   
-    char string[32];
-    printf("Diga uma palavra: ");
-    scanf("%25s", string);
+int main(void) {
+    FILE *arquivo;
+    char texto[26];
 
-    FILE* file = fopen("q1.txt", "w");
-    if (file == NULL){
-        printf("não conseguiu criar o arquivo\n");
+    printf("Digite uma string (max 25 caracteres): ");
+    fgets(texto, 26, stdin);
+
+    arquivo = fopen("texto.txt", "w");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo!\n");
+        return 1;
+    }
+    escreva_string(arquivo, texto); //a função escreve a string no arquivo
+
+    fclose(arquivo);
+    printf("String gravada em 'texto.txt'!\n");
+    return 0;
+}
+
+//para a questao 2
+
+#include <stdio.h>
+#include <string.h>
+
+void escreva_string(FILE* f, char* str) {
+    for (int i = 0; i < strlen(str); i++) {
+        fputc(str[i], f);
+    }
+}
+
+int main(void) {
+    FILE* file = fopen("q4.txt", "w");
+    if (file == NULL) {
+        printf("Arquivo não criado\n");
         return 1;
     }
 
-    escreva_string (file, string);
-    fclose(file);
-}
-void escreva_string (FILE* f, char* str){
-    int size = strlen(str);
-    for (int i = 0; i < size; i++){
-        fputc(str[i], f); // equivalente a escrever com fprintf(f, "%c", str[i]), mas mais simples e direto, c é cactere tipo char
+    char strings[10][26];    //10 strings, cada uma com até 25 caracteres + '\0', como se fosse 10 gavetas(linhas), e 26 colunas(as letras que formam as palavras dentro das gavetas)
+    
+    for (int i = 0; i < 10; i++) {   //leitura das 10 strings
+        printf("Digite uma palavra (máx 25 caracteres): ");
+        scanf("%25s", strings[i]);  // lê uma string e guarda em strings[i]
     }
+
+    for (int i = 0; i < 10; i++) {
+        escreva_string(file, strings[i]);
+        fprintf(file, "\n");
+    }
+
+    fclose(file);
+    printf("Palavras gravadas em 'q4.txt'!\n");
+
+    return 0;
 }
